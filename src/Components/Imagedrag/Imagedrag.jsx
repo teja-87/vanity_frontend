f0120e4438cd0285db4e39af78942e0112102bf3
teja <tejasaiadeveloper@gmail.com>
2025-12-31 23:13:18 +0530

    img

--- src/Components/Imagedrag/Imagedrag.jsx
+++ src/Components/Imagedrag/Imagedrag.jsx
@@ -1,14 +1,27 @@
 import React,{useState,useEffect,useRef} from "react";
 
 const Imagedrag =()=>{
+
+
+    const inputRef = useRef("null");
+    const [isDrag, setisDrag] = useState(true);
+    const [isFile,setisFile] = useState(true);
+    const handleInput=()=>{
+
+    };
+    
     return(
 
-          <div className="inputimage">
-                <p id="pimg">Click Here or Drag and Drop Image Here</p>
-                <input accept=".jpeg,.webp,.png,.img" type="file" hidden />
-            </div>
+
+        <div className="inputimage" 
+        onClick={()=>inputRef.current.click()}>
+            <p id="pimg">Click Here or Drag and Drop Image Here</p>
+            <input ref={inputRef} accept=".jpeg,.webp,.png,.img" type="file" hidden />
+        </div>
 
     )
 }
+
+
 
 export default Imagedrag;
