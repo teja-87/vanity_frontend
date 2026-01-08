936f06773a15f19f6a7b81d087d42c6f914dd07c
teja <tejasaiadeveloper@gmail.com>
2026-01-08 22:04:19 +0530

    bg changes

--- src/pages/Launchpad/Launchpad.jsx
+++ src/pages/Launchpad/Launchpad.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../Components/navbar/Navbar';
 import Launchform from '../../Components/Launchpadform/Launchform';
 
 const Launchpad=()=>{
+
+    useEffect(()=>
+    document.body.style.backgroundColor='white'
+      
+    
+
+        );
 return(
 <>
     <Navbar/>
