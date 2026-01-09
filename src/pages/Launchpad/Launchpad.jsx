5af0037e5218f0ad51e2ccd95e3de5ed84a57895
teja <tejasaiadeveloper@gmail.com>
2026-01-09 19:19:16 +0530

    bg css

--- src/pages/Launchpad/Launchpad.jsx
+++ src/pages/Launchpad/Launchpad.jsx
@@ -4,12 +4,15 @@ import Launchform from '../../Components/Launchpadform/Launchform';
 
 const Launchpad=()=>{
 
-    useEffect(()=>
-    document.body.style.backgroundColor='white'
+//     useEffect(()=>{
+//     document.body.style.backgroundColor='white';
+//     return()=>{
+//         document.body.style.background='linear-gradient(35deg, #1861ff, #b34700)';
+//     };
       
     
 
-        );
+//  } ,[]);
 return(
 <>
     <Navbar/>
